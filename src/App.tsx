ab2aae2aff2ba6a38bbf40c1f5b18ae632c1198b
tesasoftwareindia898 <tesasoftwareindia@gmail.com>
2026-08-27 03:44:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { Home } from "@/pages/home";
-import { Order } from "@/pages/order";
+import { Home } from "./pages/home";
+import { Order } from "./pages/order";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" component={Home} />
         <Route path="/order/:id" component={Order} />
       </Switch>
-      <Toaster />
     </QueryClientProvider>
   );
 }
